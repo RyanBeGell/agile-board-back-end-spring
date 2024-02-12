a3970c13ccaac1753c29527c0a14c642c5cc384f
RyanBeGell <Ryanbegell@outlook.com>
2024-02-12 18:38:23 -0500

    Initial commit

---
+++ src/main/java/dev/ryan/AgileBoardBackEndSpring/AgileBoardBackEndSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.ryan.AgileBoardBackEndSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileBoardBackEndSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgileBoardBackEndSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ryan/AgileBoardBackEndSpring/AgileBoardBackEndSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ryan.AgileBoardBackEndSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileBoardBackEndSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
