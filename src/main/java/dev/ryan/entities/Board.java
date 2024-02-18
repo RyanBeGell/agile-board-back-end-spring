5f4f2391c561e48e3562873c6f7ff3186af89057
RyanBeGell <Ryanbegell@outlook.com>
2024-02-17 23:40:15 -0500

    Board, Card, Column, and User entities created

---
+++ src/main/java/dev/ryan/entities/Board.java
@@ -0,0 +1,25 @@
+package dev.ryan.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @ManyToMany(mappedBy = "boards", fetch = FetchType.LAZY)
+    private Set<User> users = new HashSet<>();
+}
---
+++ src/main/java/dev/ryan/entities/User.java
@@ -0,0 +1,33 @@
+package dev.ryan.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+//Lombok
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "user_boards",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "board_id"))
+    private Set<Board> boards = new HashSet<>();
+}
