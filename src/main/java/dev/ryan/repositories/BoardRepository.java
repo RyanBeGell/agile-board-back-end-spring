408e5c69625642e189d51c6533dd527f963a22c3
RyanBeGell <Ryanbegell@outlook.com>
2024-02-17 23:50:49 -0500

    Board, Card, Column, and User repositories created

---
+++ src/main/java/dev/ryan/repositories/BoardRepository.java
@@ -0,0 +1,10 @@
+package dev.ryan.repositories;
+
+import dev.ryan.entities.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+}
